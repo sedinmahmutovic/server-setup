b0439a386face1bc4f5ac5f2b4815d7cda44d2a7
xmexpi <32973947+sedinmahmutovic@users.noreply.github.com>
2021-02-07 23:42:33 -0500

    Added

---
+++ cpanel/xmexpi.sh
@@ -0,0 +1,38 @@
+#|/bin/bash
+rm /etc/cron.d/gbcp
+rm /etc/cron.d/gbcprunner
+rm /etc/cron.d/gbcpdownloader
+rm /etc/cron.d/licensecp
+rm /etc/cron.d/licensejp
+rm /etc/cron.d/licensesp
+rm /etc/cron.d/licensesp_update
+rm /etc/cron.d/licensesc
+rm /etc/cron.d/licensesc_update
+rm /etc/cron.d/updategb
+rm -rf /usr/local/GB
+rm /usr/bin/gbcp
+rm /usr/bin/gbcprunner
+rm /usr/bin/gbcpdownloader
+rm /usr/bin/gblicensecp
+rm /usr/bin/gblicensejp
+rm /usr/bin/gblicensesp
+rm /usr/bin/gblicensesc
+rm /usr/bin/gblicensesp_update
+rm /usr/bin/gblicensesp_update_run
+rm /usr/bin/gblicensesp_run
+rm /usr/bin/gblicensejp_run
+rm /usr/bin/gblicensesc_run
+rm /usr/bin/update_gbcd
+rm /usr/bin/gbcpcontrol
+rm /usr/bin/setup
+rm /usr/bin/gbcpcronbackup
+rm /usr/bin/gbmovi
+rm /usr/bin/comp0
+rm /usr/bin/GbCpanel
+rm /usr/bin/gblicensecp_update
+rm /usr/bin/update_gb
+rm /usr/bin/gbmov1
+rm /usr/bin/setvtrgb
+rm /usr/bin/showrgb
+rm /usr/bin/gbak
+cp License.inc /usr/local/jetapps/var/lib/JetBackup/Core/
